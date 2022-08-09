93e3e54a6a8133b4f99df8d40ce8dcab57a11c70
Nespasibo <Nikita.Brachun@playtech.com>
2022-08-09 19:28:59 +0300

    first safe hello txt

---
+++ src/test/java/java/guru/Firsttest.java
@@ -0,0 +1,16 @@
+package src.test.java.java.guru;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Firsttest {
+    Test
+    void firstTest() {
+
+        Assertions.assertTrue(true);
+    }
+    Test
+    void secondTest() {
+        open("google.com")
+    }
+}
